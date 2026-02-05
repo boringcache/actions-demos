db585a1f9f7ab63f429fe9d017dc86cbfb9d67b5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-02-05 19:30:31 +0000

    Add code

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task default: :test
+
+task :test do
+  puts "Tests passed"
+end
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("BoringCache Demo");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
